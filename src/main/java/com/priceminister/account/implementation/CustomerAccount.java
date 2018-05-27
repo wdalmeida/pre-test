c6a697680c458292e8e5d984e07f7e1e58400827
Warren D'ALMEIDA <warren.dalmeida@gmail.com>
2018-05-27 22:34:46 +0200

    implementation getBalance

--- src/main/java/com/priceminister/account/implementation/CustomerAccount.java
+++ src/main/java/com/priceminister/account/implementation/CustomerAccount.java
@@ -5,13 +5,18 @@ import com.priceminister.account.*;
 
 public class CustomerAccount implements Account {
 
+    private Double balance;
+
+    public CustomerAccount() {
+        this.balance=0.0;
+    }
+
     public void add(Double addedAmount) {
         // TODO Auto-generated method stub
     }
 
     public Double getBalance() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.balance;
     }
 
     public Double withdrawAndReportBalance(Double withdrawnAmount, AccountRule rule) 

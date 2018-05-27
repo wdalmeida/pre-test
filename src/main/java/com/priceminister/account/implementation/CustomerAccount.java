baf88a15bae3d5f80c380129ad8370e9898ebd00
Warren D'ALMEIDA <warren.dalmeida@gmail.com>
2018-05-27 23:09:33 +0200

    Add logger and implement method add

--- src/main/java/com/priceminister/account/implementation/CustomerAccount.java
+++ src/main/java/com/priceminister/account/implementation/CustomerAccount.java
@@ -2,8 +2,13 @@ package com.priceminister.account.implementation;
 
 import com.priceminister.account.*;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class CustomerAccount implements Account {
+
+    private final Logger logger = Logger.getLogger(CustomerAccount.class.getName());
 
     private Double balance;
 
@@ -12,15 +17,17 @@ public class CustomerAccount implements Account {
     }
 
     public void add(Double addedAmount) {
-        // TODO Auto-generated method stub
+        logger.log(Level.INFO,"add {0} to the balance",addedAmount);
+        this.balance += addedAmount;
     }
 
     public Double getBalance() {
+        logger.log(Level.INFO,()->"the balance is "+ this.balance);
         return this.balance;
     }
 
-    public Double withdrawAndReportBalance(Double withdrawnAmount, AccountRule rule) 
-    		throws IllegalBalanceException {
+    public Double withdrawAndReportBalance(Double withdrawnAmount, AccountRule rule)
+            throws IllegalBalanceException {
         // TODO Auto-generated method stub
         return null;
     }
